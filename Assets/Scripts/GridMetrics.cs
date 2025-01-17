b51096a35189f145fae762629c485ad8a33bc54d
NotADev9000 <NotADev9000@users.noreply.github.com>
2025-01-17 16:10:34 +0000

    initial commit

---
+++ Assets/Scripts/GridMetrics.cs
@@ -0,0 +1,5 @@
+﻿public static class GridMetrics
+{
+	public const int NumThreads = 8;
+	public const int PointsPerChunk = 16;
+}
---
+++ Assets/Scripts/NoiseGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NoiseGenerator : MonoBehaviour
+{
+    [Header("Noise Shader")]
+    [SerializeField] private ComputeShader NoiseShader;
+
+    [Space()]
+    [Header("Noise Settings")]
+    [SerializeField] float noiseScale = 0.08f;
+    [SerializeField] float amplitude = 200;
+    [SerializeField] float frequency = 0.004f;
+    [SerializeField] int octaves = 6;
+    [SerializeField, Range(0f, 1f)] float groundPercent = 0.2f;
+
+    [Space()]
+    [Header("Chunk to Update")]
+    [SerializeField] private MeshGenerator _chunk;
+
+    private ComputeBuffer _weightsBuffer;
+    // Unity throws a fit if you try to call a compute shader from OnValidate so we need to do it in Update
+    private bool _doValidateBehaviour = false;
+
+    private void Awake()
+    {
+        CreateBuffers();
+    }
+
+    private void OnDestroy()
+    {
+        ReleaseBuffers();
+    }
+
+    private void Update()
+    {
+        if (_doValidateBehaviour)
+        {
+            _chunk.Execute();
+            _doValidateBehaviour = false;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (EditorApplication.isPlaying && _chunk != null)
+        {
+            _doValidateBehaviour = true;
+        }
+    }
+
+    public float[] GetNoise()
+    {
+        float[] noiseValues =
+            new float[GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk];
+
+        NoiseShader.SetBuffer(0, "_Weights", _weightsBuffer);
+
+        NoiseShader.SetInt("_ChunkSize", GridMetrics.PointsPerChunk);
+        NoiseShader.SetFloat("_NoiseScale", noiseScale);
+        NoiseShader.SetFloat("_Amplitude", amplitude);
+        NoiseShader.SetFloat("_Frequency", frequency);
+        NoiseShader.SetInt("_Octaves", octaves);
+        NoiseShader.SetFloat("_GroundPercent", groundPercent);
+
+        int numThreadGroups = Mathf.CeilToInt(GridMetrics.PointsPerChunk / GridMetrics.NumThreads);
+        NoiseShader.Dispatch(
+            0, numThreadGroups, numThreadGroups, numThreadGroups
+        );
+
+        _weightsBuffer.GetData(noiseValues);
+
+        return noiseValues;
+    }
+
+    private void CreateBuffers()
+    {
+        _weightsBuffer = new ComputeBuffer(
+            GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk, sizeof(float)
+        );
+    }
+
+    private void ReleaseBuffers()
+    {
+        _weightsBuffer.Release();
+    }
+}
